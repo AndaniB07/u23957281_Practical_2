ad2671fe2bcf42410223dcc06bf36320cabe665b
ANDANI07 <149280110+ANDANI07@users.noreply.github.com>
2025-03-10 23:08:01 +0200

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u23957281_Practical_2.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult StudentGroup()
+        {
+            List<Models.StudyGroup> students = new List<Models.StudyGroup>();
+            students.Add(new Models.StudyGroup { StudentNo = "u12345678", FirstName = "Jane", LastName = "Doe", EmailAddress = "jane.doe@tuks.co.za", myLink = "~/HTML/Jane.html" });
+            students.Add(new Models.StudyGroup { StudentNo = "u01234567", FirstName = "John", LastName = "Doe", EmailAddress = "john.doe@tuks.co.za", myLink = "~/HTML/John.html" });
+            students.Add(new Models.StudyGroup { StudentNo = "u23957281", FirstName = "Andani", LastName = "Bologo", EmailAddress = "u23957281@tuks.co.za", myLink = "~/HTML/Andani.html" });
+            students.Add(new Models.StudyGroup { StudentNo = "u23619270", FirstName = "Vhuthuhawe", LastName = "Negondeni", EmailAddress = "u23619270@tuks.co.za", myLink = "~/HTML/Vhuthuhawe.html" });
+            students.Add(new Models.StudyGroup { StudentNo = "u23854830", FirstName = "Dakalo", LastName = "Nemamilwe", EmailAddress = "u23854830@tuks.co.za", myLink = "~/HTML/Dakalo.html" });
+
+
+            return View(students);
+
+        }
+    }
+}
---
+++ Models/StudyGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.ExceptionServices;
+
+namespace u23957281_Practical_2.Models
+{
+    public class StudyGroup
+    {
+
+        [Display(Name = "Student Number")]
+        public string StudentNo { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Link to personal page")]
+        public string myLink { get; set; }
+    }
+}
